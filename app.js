fb34eba43e4d534e8243ff858bc02f31fca6a489
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2024-03-06 18:19:25 +0100

    created all DOM variables

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 "use strict"
+
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const checkUserInput = () => {
+    
+}
 const binaryArray = [];
 const binaryConverter = (n) => {
     if(n === 0){
-        return []
+        return [];
     }else{
         binaryConverter(~~(n/2)) // getting the quotient of the dividend
         binaryArray.push(n%2) // getting the reminder
-        return binaryArray;
+        return binaryArray.toString();
     }
 }
-console.log(binaryConverter(15));
+console.log(binaryConverter(150));
